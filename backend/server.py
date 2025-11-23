20db9f8c3b0b90d0e1cccb16b35cafa9cf3d8008
Taizeez <abdulazeezshakrullah@gmail.com>
2025-11-23 14:58:55 +0100

    set port dynamically

--- backend/server.py
+++ backend/server.py
@@ -9134,4 +9134,5 @@ async def find_available_drivers(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
